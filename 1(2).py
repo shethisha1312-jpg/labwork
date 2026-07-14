777635d7ffee968893dd185e49742b0928a1ab63
shethisha1312-jpg <shethisha1312@gmail.com>
2026-07-14 22:43:59 +0530

    secondpro

---
+++ 1(2).py
@@ -0,0 +1,77 @@
+# Q1) diffrent formatingoptions in print()
+
+# print("One","Two","Three",sep="-")
+
+# print("python",end=" ")
+# print("c++")
+
+# Q2) 
+
+# name=input("Enter Name:")
+# age=input("Enter Age:")
+# hobby=input("Enter your hobby:")
+
+# print("Hello",name + "!","At",age + ",","Enjoying",hobby,"sounds fun!")
+
+# Q3)  Arithmatic operation 
+
+# a = int(input("Enter First number :"))
+# b=int(input("Enter second number :"))
+
+# print("Addition is => ",a+b)
+# print("Subtraction is =>",a-b)
+# print("Multiplication is =>",a*b)
+# print("Division is =>",a/b)
+# print("Moduls is =>",a % b)
+# print("Floor Division =>",a//b)
+# print("Exponat =>",a**b)
+
+# Q4) data types 
+
+# a = 21
+# b = 21.9
+# c = "Isha"
+# d = True
+# e = 2 + 3j
+
+# print(a,type(a))
+# print(b,type(b))
+# print(c,type(c))
+# print(d,type(d))
+# print(e,type(e))
+
+# Q5) 
+
+# height=input("Enter your Height :")
+# weight=input("Enter your wight :")
+
+# print("Your Height is ",height)
+# print("Your Wight is",weight)
+
+# Q6) Logical operators
+
+# age = int(input("Enter your age: "))
+# marks = int(input("Enter your marks: "))
+
+# print("AND =", age >= 18 and marks >= 35)
+# print("OR =", age >= 18 or marks >= 35)
+# print("NOT =", not(age >= 18))
+
+# Q7) assignment operator
+
+# a = 10
+
+# print("a =", a)
+
+# a += 5
+# print("a += 5 =", a)
+
+# a -= 3
+# print("a -= 3 =", a)
+
+# a *= 2
+# print("a *= 2 =", a)
+
+# a /= 4
+# print("a /= 4 =", a)
+
