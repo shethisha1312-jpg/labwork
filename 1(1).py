8291260bf54fd30eaf9fbcd3d3ed6787e2da1c4e
shethisha1312-jpg <shethisha1312@gmail.com>
2026-07-14 22:05:58 +0530

    firstpro

---
+++ 1(1).py
@@ -0,0 +1,21 @@
+# Q1) allready perfomed completed instolation
+
+# Q2) print your name , age and school
+
+# print("My Name is Isha ")
+# print("Age is 21")
+# print("School name is sent marry high school")
+
+
+# Q3) pattern
+
+# *
+# **
+# ***
+# **
+# *
+
+# for i in range(1,4):
+#     print("*" * i)
+# for i in range(2,0,-1):
+#     print("*" * i)
