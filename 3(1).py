59e35d409451c1a4d240f307cc6c21944df410bf
shethisha1312-jpg <shethisha1312@gmail.com>
2026-07-20 14:42:28 +0530

    Datatype in ditail

---
+++ 3(1).py
@@ -0,0 +1,66 @@
+# Q1) take a user first namd and last name as input . print them in the formate 
+
+# firstname=input("Enter first name :")
+# lastname=input("Enter last name :")
+
+# print(f'Hello {lastname},{firstname}')
+
+# Q2) 
+
+# item = "apple"
+# price = 5.50
+
+# print(f'The price of {item} is {price} dollars ')
+
+# Q3) 
+
+# str = input("Enter staring :")
+
+# revered = str[::-1]
+
+# print("Reversed is :",revered)
+
+# if str == revered:
+#     print("It Is palindrome ")
+# else:
+#     print("It Is not plindrome")
+
+
+# Q4) 
+
+# str  = input("Enter any string :")
+
+# print("Uppercase :",str.upper())
+# print("Lowercase :",str.lower())
+# print("Title :",str.title())
+
+# Q5) 
+
+# sentence = " Machine Learning and AI are treanding"
+# b=sentence.split()
+# b[3] = "Artifical Inteligentce"
+
+# print(" ".join(b))
+
+# Q6) split string into list
+
+# text = "apple,banana,grapes"
+
+# fruits = text.split(",")
+# print(fruits)
+
+# words=["Python", "is", "awesome"]
+
+# sentence =""
+
+# for word in words:
+#     sentence=sentence + word + " "
+
+# print(sentence)
+
+
+
+# Q7) check if a string starts with "Hello"and ends with "World"
+#     - remove all non-alphabetic charachter from "Data123#science !"
+#     - reverce the string "python"
+
