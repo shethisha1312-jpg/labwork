4ab127d4532333d1f901dc587667e3fd60542524
shethisha1312-jpg <shethisha1312@gmail.com>
2026-07-21 12:20:48 +0530

    datatypes in detail

--- 3(1).py
+++ 3(1).py
@@ -44,19 +44,19 @@
 
 # Q6) split string into list
 
-# text = "apple,banana,grapes"
+text = "apple,banana,grapes"
 
-# fruits = text.split(",")
-# print(fruits)
+fruits = text.split(",")
+print(fruits)
 
-# words=["Python", "is", "awesome"]
+words=["Python", "is", "awesome"]
 
-# sentence =""
+sentence =""
 
-# for word in words:
-#     sentence=sentence + word + " "
+for word in words:
+    sentence=sentence + word + " "
 
-# print(sentence)
+print(sentence)
 
 
 
@@ -64,3 +64,52 @@
 #     - remove all non-alphabetic charachter from "Data123#science !"
 #     - reverce the string "python"
 
+# 1. Check if string starts with "Hello" and ends with "World"
+
+# text = input("Enter a string: ")
+
+# if text.startswith("Hello") and text.endswith("World"):
+#     print("String starts with Hello and ends with World")
+# else:
+#     print("Condition not satisfied")
+
+
+# 2. Remove all non-alphabetic characters
+
+# first way to solve this 
+#============================
+# text = "Data123#Science!"
+
+# result = ""
+
+# for ch in text:
+#     if ch.isalpha():
+#         result = result + ch
+
+# print(result)
+
+#====================================
+
+# second way to solve this program 
+#====================================
+# str="Data123#Science!"
+# strlest=(list(str))
+# stralpha=""
+# for i in strlest:
+#     if(i.isalpha()):
+#         stralpha+= i
+    
+# print(stralpha)
+
+
+
+# 3. Reverse the string "Python"
+
+# text = "Python"
+
+# reverse = ""
+
+# for ch in text:
+#     reverse = ch + reverse
+
+# print(reverse)
