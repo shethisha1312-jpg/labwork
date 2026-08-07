22466fa810b69b6210d57e1507dc9a77acf92f98
AnkitBerani <v-ankit.berani@rebelfoods.com>
2026-08-07 12:42:22 +0530

    1D array

---
+++ 4(3).py
@@ -0,0 +1,81 @@
+# 1) create a 1D array with five intiger element and display the array using loop
+
+# arry = [1,2,3,4,5]
+# for i in arry:
+#     print(i)
+
+# 2) Develop a program to calculate the sum of all elements in a 1D array
+
+# array = [1,2,3,4,5]
+# sum =0
+# for i in array:
+#     sum += i
+# print("Sum of array elements ",sum)
+
+# 3) create a program to insert a new element at a specific position in 1D array 
+
+# array = [12,4,34,89]
+
+# print(array)
+# value = int(input ("Enter a new value :"))
+# position = int(input("Enter position "))
+
+# array.insert(position,value)
+
+# print("Update array :")
+# print(array)
+
+# 4) write a program to delete an element by its value from an 1D array
+
+# array = [12,4,34,89]
+# print(array)
+# value = int(input("Enter the value to delete: "))
+# array.remove(value)
+# print("Updated array:")
+# print(array)
+
+# array = [12,4,34,89]
+# print(array)
+
+# value = int(input("Enter the value to delete: "))
+# if value in array:
+#     index = array.index(value)
+#     del array[index]
+#     print("Updated array:",array) 
+# else:
+#     print("value not found ")
+
+# 5) devlop a program to update an element in a 1D array by its index
+
+# array = [1,4,6,7,89]
+
+# print(array)
+# index = int(input("Enter the index to update:"))
+# value = int(input("Enter the new value:"))
+# array[index]=value
+# print("Updated array :")
+# print(array)
+
+# 6) implement a program to serch for an element in a 1D array and return its index 
+
+# array = [1,4,6,7,89]
+
+# print(array)
+
+# value = int(input("Enter the value to search:"))
+# if value in array:
+#     print("Element found at index:",array.index(value))
+    
+# else:
+#     print("Element not found in the array")
+    
+
+# 7) write a program to concatenate two 1D array in to a single array 
+
+arr1 = [1,2,3]
+arr2 = [9,10]
+
+print(arr1 + arr2) 
+    
+
+
